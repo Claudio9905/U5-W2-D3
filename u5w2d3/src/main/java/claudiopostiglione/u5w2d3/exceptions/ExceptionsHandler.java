ba609c4b55cb034e465bc446dc61f91953cd746d
Claudio9905 <cloudpost10@gmail.com>
2025-10-16 09:21:33 +0200

    Update File

--- u5w2d3/src/main/java/claudiopostiglione/u5w2d3/exceptions/ExceptionsHandler.java
+++ u5w2d3/src/main/java/claudiopostiglione/u5w2d3/exceptions/ExceptionsHandler.java
@@ -3,28 +3,29 @@ package claudiopostiglione.u5w2d3.exceptions;
 
 import claudiopostiglione.u5w2d3.payloads.ErrorsPayload;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDate;
 
 @RestControllerAdvice
-public class ExceptionHandler extends RuntimeException {
+public class ExceptionsHandler extends RuntimeException {
 
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(BadRequestException.class)
+    @ExceptionHandler(BadRequestException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorsPayload handleBadRequest(BadRequestException ex) {
         return new ErrorsPayload(ex.getMessage(), LocalDate.now());
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(IdNotFoundException.class)
+    @ExceptionHandler(IdNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorsPayload handleIdNotFound(IdNotFoundException er){
         return new  ErrorsPayload(er.getMessage(), LocalDate.now());
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
+    @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorsPayload handleServerError(Exception er){
         return new  ErrorsPayload(er.getMessage(), LocalDate.now());
